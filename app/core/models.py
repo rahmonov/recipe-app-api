e708fbed91c4123c7ad0e0d90143c984d2b5e430
Jakhongir Rakhmonov <jakhongir.rakhmonov@deliveryhero.at>
2019-10-17 14:30:47 +0200

    added working list tags endpoint/

--- app/core/models.py
+++ app/core/models.py
@@ -40,7 +40,9 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Tag(models.Model):
     name = models.CharField(max_length=255)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.name
