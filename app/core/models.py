c74252d30216846bce226acd48034130453cf667
Jakhongir Rakhmonov <jakhongir.rakhmonov@deliveryhero.at>
2019-10-17 14:01:38 +0200

    added recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin,
 )
+from django.conf import settings
 
 
 class UserManager(BaseUserManager):
@@ -35,3 +36,11 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='test@gmail.com', password='testpass'):
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTest(TestCase):
     def test_create_user_with_email_successful(self):
@@ -34,3 +40,10 @@ class ModelTest(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+        self.assertEqual(str(tag), tag.name)
