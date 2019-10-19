dd96c30202cc7c6062c87ef32a54e4280bd64f7a
Jahongir Rahmonov <jrahmonov2@gmail.com>
2019-10-19 23:21:44 +0200

    filtering done

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -15,7 +15,15 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
